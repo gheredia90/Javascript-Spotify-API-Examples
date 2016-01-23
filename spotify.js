f9ab6d667f1a2b1d14a167ec449d4eede8a604ec
gheredia90 <gheredia28@gmail.com>
2016-01-23 14:34:22 +0100

    version 2.0

--- spotify.js
+++ spotify.js
@@ -1,4 +1,5 @@
 var API_URL = 'https://api.spotify.com/v1/search?type=artist&query=';
+var API_BASE_URL = 'https://api.spotify.com'
 
 $('.form-horizontal').on('submit', function(event){
 	event.preventDefault();
@@ -14,7 +15,6 @@ $('.form-horizontal').on('submit', function(event){
         error: handleError,
         dataType: "json"
     });   
-
 });
 
 function showSearchResults (response) {
@@ -30,6 +30,7 @@ function showSearchResults (response) {
 function handleError (error) {
     console.log(error);
 }
+
 function addArtist(artist, index){
 	var name = artist.name;
 	var image = artist.images[2];
@@ -37,19 +38,53 @@ function addArtist(artist, index){
 
 	if(image){
 		$('.list-group').append('<li class="list-group-item"><img src='
-		+ image.url + ' height="70">' + " "
-		+ (index+1) + ".- " + name
+		+ image.url + ' height="70">' 
+		+ '<p class="artist" data-toggle="modal" id="'
+		+ id + '"' + '>'
+		+ (index+1) 
+		+ ".- " + name	
+		+ '</p>'	
 		+ '</li>');
 	} else {
 		$('.list-group').append('<li class="list-group-item">'
+		+ '<p class="artist" data-toggle="modal" id="'
+		+ id + '"' + '>'
 		+ (index+1) + ".- " + name
 		+ '</li>');
 
 	}	
 }
 
-$('.list-group.item').on('click', function(event){
-	event.preventDefault();
-	console.log('vamos a llamar de nuevo')
 
-})
+$( "body" ).on( "click", "p", function() {
+    
+	var id = $(this).attr('id');
+	var albums_url = API_BASE_URL + "/v1/artists/"
+	+ id + "/albums";
+
+	$.ajax({
+	    type: "GET",
+	    url: albums_url,
+	    data: '',
+	    success: showAlbums,
+	    error: handleError,
+	    dataType: "json"
+	});
+});	
+
+function showAlbums (response) {
+    var albums =  response.items;
+    $('.album-list').empty();
+
+    albums.forEach(function(album, index){
+    	addAlbum(album);
+    });
+    $("#albumsModal").modal();
+}
+
+function addAlbum(album){
+	var name = album.name;
+	$('.album-list').append('<li class="list-group-item">'
+		+ name	
+	+ '</li>');		
+}
